e4f3b3c120f86382c7e6e75e12b82feac9fed1f7
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2024-05-30 10:06:57 +0700

    mobile removed

--- mobilelmaochat/contexts/SocketContext.js
+++
@@ -1,9 +0,0 @@
-/*
-  * Used by 
-  *   index.js
-*/
-
-import { createContext } from "react";
-import GlobalVariables from "./../GlobalVariables";
-
-export default createContext(GlobalVariables.socket);
