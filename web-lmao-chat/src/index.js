9b326009afb09e43ce8633df774c75e771541c80
LeeTrongjNghiax <leetrongjnghiax0938225745@gmail.com>
2024-06-08 13:00:36 +0700

    web added code consistency 2

--- web-lmao-chat/src/index.js
+++ web-lmao-chat/src/index.js
@@ -1,47 +1,47 @@
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 import SocketContext from "./contexts/SocketContext";
 import ThemeProvider from "./contexts/ThemeProvider";
-import ErrorPage from './pages/ErrorPage.tsx';
-import MainPage from './pages/MainPage.tsx';
-import OTPVerifyPage from './pages/OTPVerifyPage.tsx';
-import ResetPasswordPage from './pages/ResetPasswordPage.tsx';
-import SignInPage from './pages/SignInPage.tsx';
-import SignUpPage from './pages/SignUpPage.tsx';
+import ErrorPage from "./pages/ErrorPage.tsx";
+import MainPage from "./pages/MainPage.tsx";
+import OTPVerifyPage from "./pages/OTPVerifyPage.tsx";
+import ResetPasswordPage from "./pages/ResetPasswordPage.tsx";
+import SignInPage from "./pages/SignInPage.tsx";
+import SignUpPage from "./pages/SignUpPage.tsx";
 import GlobalVariables from "./GlobalVariables";
-import './index.css';
+import "./index.css";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: `/`,
     element: (<SignInPage />),
     errorElement: <ErrorPage />,
   },
   {
-    path: "/SignUpPage",
+    path: `/SignUpPage`,
     element: (<SignUpPage />),
     errorElement: <ErrorPage />,
   },
   {
-    path: "/OTPVerifyPage",
+    path: `/OTPVerifyPage`,
     element: (<OTPVerifyPage />),
     errorElement: <ErrorPage />,
   },
   {
-    path: "/ResetPasswordPage",
+    path: `/ResetPasswordPag`,
     element: (<ResetPasswordPage />),
     errorElement: <ErrorPage />,
   },
   {
-    path: "/MainPage",
+    path: `/MainPage`,
     element: (<MainPage />),
     errorElement: <ErrorPage />,
   },
 ]);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById(`root`));
 root.render(
   // <React.StrictMode>
     <ThemeProvider>
